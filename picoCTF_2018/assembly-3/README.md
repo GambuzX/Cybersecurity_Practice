ba79506dccfa1021aee0f49a62fe995c246ab0a0
GambuzX <mg_mesquita@hotmail.com>
2019-02-19 10:00:48 +0000

    Solved assembly 3; Renamed got-shell? to got-shell

---
+++ picoCTF_2018/assembly-3/README.md
@@ -0,0 +1,3 @@
+I reproduced the assembly instructions in the asm_3.py script.
+
+Running it you get the answer 0x478.
--- picoCTF_2018/assembly-3/asm_3.py
+++ picoCTF_2018/assembly-3/asm_3.py
@@ -7,39 +7,33 @@ def get_byte(reg, n):
 	shift = (n-1)*8
 	return (reg & (mask << shift)) >> shift
 
-def ah(reg):
+def higher8(reg):
 	return (reg & 0xff00) >> 8
 
-def al(reg):
+def lower8(reg):
 	return reg & 0xff
 
 eax = 0xbc
 
-eax = ah(eax) << 8
+eax = higher8(eax) << 8
 
 temp = get_byte(arg1, 2)
-eax = (temp << 8) | al(eax)
+eax = (temp << 8) | lower8(eax)
 
-temp_eaxl = eax | 0xffff0000
+temp_eaxl = eax & 0xffff0000
 eax = eax << 0x10
 eax = temp_eaxl | (eax & 0xffff)
 
 temp = get_byte(arg2, 1)
-temp = al(eax) - temp
-eax = (eax & 0xffffff00) | temp
+temp = lower8(eax) - temp
+eax = (eax & 0xffffff00) | lower8(temp)
 
 temp = get_byte(arg2, 2)
-temp = ah(eax) + temp
-eax = (eax & 0xffff00ff) | temp
+temp = higher8(eax) + temp
+eax = (eax & 0xffff00ff) | (lower8(temp) << 8)
 
 temp = (get_byte(arg3, 2) << 8) | get_byte(arg3, 1)
-temp_eaxl = eax | 0xffff0000
+temp_eaxl = eax & 0xffff0000
 eax = temp_eaxl | ( (eax & 0xffff) ^ temp)
 
-print(eax)
 print(hex(eax))
-
-
-
-
-# CHECK IF BYTES ARE CORRECT!!!! LITTLE ENDIAN VS BIG ENDIAN
